b4605ed2b5e2a0c8c4fd641f24bb0e78c609f563
Alex Ulbrich <alex@ulbrichtech.com>
2018-02-12 10:29:36 -0500

    Create ChocoWISBase.ps1

---
+++ Utility/ChocoWISBase.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy Bypass -Scope process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install GoogleChrome -y
+choco install gotomeeting -y
+choco install slack -y
+choco install adobereader -y
+choco install flashplayerplugin -y
+choco install jre8 -y
+choco install webexplayer -y
+choco install lastpass -y
+choco install teamviewer -y
