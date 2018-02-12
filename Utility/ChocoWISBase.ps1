4408270f38612836f64d9943c76dd7f3f891041c
Alex Ulbrich <alex@ulbrichtech.com>
2018-02-12 11:00:21 -0500

    Update ChocoWISBase.ps1

--- Utility/ChocoWISBase.ps1
+++ Utility/ChocoWISBase.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install GoogleChrome -y
 choco install slack -y
-choco install adobereader -y
+choco install FoxitReader -y
 choco install flashplayerplugin -y
 choco install jre8 -y
 choco install webexplayer -y
