7ec7acfbcce52c8d3fcfc8b597f3a2720beca191
Alex Ulbrich <alex@ulbrichtech.com>
2020-10-22 15:10:50 -0400

    removed obsoleted code

--- Utility/ChocoWISBase.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install GoogleChrome -y
-choco install slack -y
-choco install FoxitReader -y
-choco install flashplayerplugin -y
-choco install jre8 -y
-choco install lastpass -y --ignore-checksums
-choco install teamviewer -y
